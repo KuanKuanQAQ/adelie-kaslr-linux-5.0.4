f5960e423d908f2dcda2b7ef5ce3a981f8acf08f
KuanKuanQAQ <leaker233@icloud.com>
2023-06-07 06:58:16 +0000

    Boot.

---
+++ rootfs/boot_qemu.sh
@@ -0,0 +1,6 @@
+sh mkrootfs.sh
+qemu-system-x86_64 -s -S \
+    -kernel ../linux-5.0.4/arch/x86_64/boot/bzImage \
+    -initrd ./initramfs.cpio.gz \
+    -nographic \
+    -append "nokaslr console=ttyS0"
